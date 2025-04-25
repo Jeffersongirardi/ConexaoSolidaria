61ccdc3f9fbe5cbcec630b7343426d6836f9e767
Jeffersongirardi <157550626+Jeffersongirardi@users.noreply.github.com>
2025-04-25 16:04:41 -0300

    Atulizando estruturas
    teste

--- projetoex/app.py
+++ projetoex/app.py
@@ -1,18 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
+import os
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='.')
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return send_from_directory('.', 'index.html')
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
+@app.route('/css/<path:path>')
+def send_css(path):
+    return send_from_directory('css', path)
 
-@app.route('/contact')
-def contact():
-    return render_template('contact.html')
+@app.route('/js/<path:path>')
+def send_js(path):
+    return send_from_directory('js', path)
+
+@app.route('/pages/<path:path>')
+def send_pages(path):
+    return send_from_directory('pages', path)
 
 if __name__ == '__main__':
     app.run(debug=True)
